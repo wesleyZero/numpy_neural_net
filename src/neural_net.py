170c3c67875cf62a6c71ebe8dccd6576f20f26fa
Wesley Johanson <wesleyJohansonZero@gmail.com>
2023-04-29 16:08:12 -0700

    got the forward propagation matrix dimensions consistent so that the dot product is defined

--- src/neural_net.py
+++ src/neural_net.py
@@ -8,69 +8,63 @@ from matplotlib import pyplot as plt
 	-----	
 """
 
-
-
-#read
-train_data = pd.read_csv('../assets/MNIST_CSV/train.csv')
-test_data = pd.read_csv('../assets/MNIST_CSV/test.csv')
-# print(train_data.columns)
-# print(train_data['label'])
-
-#convert
-train_data = np.array(train_data)
-test_data = np.array(test_data)
-
 #transpose 
 # train_data = train_data.T		# Each row will be an image
 # train_data = train_data.T		# Each row will be an image 
 
-m_train, n_train = train_data.shape
-m_test, n_test = test_data.shape
+# m_train, n_train = train_data.shape
+# m_test, n_test = test_data.shape
 
 
 # Train
-y = train_data[ : , 0]
-x = train_data[ : , 1:]
+# y = train_data[ : , 0]
+# x = train_data[ : , 1:]
 
 # print(y)
 
 
-
 class neural_net():
-	def __init__(self, dataset):
+	def __init__(self, training_data):
 		"""
 		Interpets the datashape as (numOfImages, 1 + numOfPixels) where the 
 		first column is the labeled data
 		"""
 		# if type(dataset) != type(np.array([])):
-		if type(dataset) is not np.array:
-			dataset = np.array(dataset)
-		self.y = dataset[ : , 0 ]
-		self.x = dataset[ : , 1 : ]
-		self.params = neural_net.init_params()
+		if type(training_data) is not np.array:
+			training_data = np.array(training_data)
+		training_data = training_data.T #Col data is now row data
+		self.y = training_data[0]		#First Row is labels	
+		self.x = training_data[1:]		#Rest of rows is training data
+		self.init_params()
 
-	@staticmethod
-	def init_params():
-		w1 = np.random.rand(10, 784)
-		b1 = np.random.rand(10, 1)
-		w2 = np.random.rand(10, 10)
-		b2 = np.random.rand(10, 1)
-		return w1, b1, w2, b2
+	def init_params(self):
+		self.w1 = np.random.rand(10, 784)
+		self.b1 = np.random.rand(10, 1)
+		self.w2 = np.random.rand(10, 10)
+		self.b2 = np.random.rand(10, 1)
+		return True
 
 	@staticmethod
 	def ReLU(x):
-		return max(0, x)
+		return np.maximum(0, x)	
+	
+	def softmax(self):
+		return 
+		
+	def forward_prop(self):
+		z1 = self.w1.dot(self.x) + self.b1
+		a1 = neural_net.ReLU(z1)
+		z2 = self.w2.dot(a1) + self.b2
+		a2 = neural_net.ReLU(z2)
 
-	def forward_prop():
-		...
 
 
-# neural_net.init_params()
+#read
+train_data = pd.read_csv('../assets/MNIST_CSV/train.csv')
+test_data = pd.read_csv('../assets/MNIST_CSV/test.csv')
+
 nn = neural_net(train_data)
-
-
-
-
+nn.forward_prop()
 
 
 
