ae68654a435006fed925a59c80f9067e15da2ea5
Wesley Johanson <wesleyJohansonZero@gmail.com>
2023-04-29 14:49:47 -0700

    added the MNIST dataset in CSV format and got it to load

--- src/neural_net.py
+++ src/neural_net.py
@@ -1,4 +1,58 @@
 import numpy as np
+import pandas as pd 
+from matplotlib import pyplot as plt 
+# import tensorflow 		# Dependency for Keras
+# import keras
+
+"""
+	-----	
+"""
 
 
-print("Hello AI")
+
+
+train_data = pd.read_csv('../assets/MNIST_CSV/train.csv')
+test_data = pd.read_csv('../assets/MNIST_CSV/test.csv')
+
+train_data = np.array(train_data)
+train_data = train_data.T		# Each column will be a image 
+m, n = train_data.shape
+print(f"{m}, {n}")
+
+
+
+np.random.shuffle(train_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("The program ran")
+
+
+
+
