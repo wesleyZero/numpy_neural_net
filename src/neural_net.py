15438e6aae66abd35cf6b51ea0dd84fb77b72bbf
Wesley Johanson <wesleyJohansonZero@gmail.com>
2023-04-29 22:53:36 -0700

    finally got it to work!

--- src/neural_net.py
+++ src/neural_net.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd 
 from matplotlib import pyplot as plt 
+import deleteMe_test as testCode
 # import tensorflow 		# Dependency for Keras
 # import keras
 
@@ -24,13 +25,14 @@ class neural_net():
 		"""
 		if type(training_data) is not np.array:
 			training_data = np.array(training_data)
+		np.random.shuffle(training_data)
 		training_data = training_data.T #Col data is now row data
 		self.y = training_data[0]		#First Row is labels	
 		self.x = training_data[1:] / 255	#Rest of rows is training data
-		length = len(self.x[0])
-		self.init_params(length)
+		# length = len(self.x[0])
+		self.init_params()
 
-	def init_params(self, length):
+	def init_params(self):
 		"""
 		Makes a neural network that is 784 -> 10(hidden layer) -> 10 
 		"""
@@ -42,13 +44,20 @@ class neural_net():
 
 	@staticmethod
 	def ReLU(Z):
-		return np.maximum(0,Z)
+		return np.maximum(Z, 0)
 
 	@staticmethod	
 	def softmax(Z):
-		top = np.exp(Z)
-		bottom = np.sum(np.exp(Z))	
-		return top / bottom
+		# top = np.exp(Z)
+		# bottom = np.sum(np.exp(Z))	
+		# return top / bottom
+		A = np.exp(Z) / sum(np.exp(Z))
+		# np.sum
+		return A
+
+	# def softmax(Z):
+		# A = np.exp(Z) / sum(np.exp(Z))
+		# return A
 
 	@staticmethod
 	def deriv_ReLU(Z):
@@ -57,7 +66,7 @@ class neural_net():
 	@staticmethod
 	def one_hot(Y):
 		one_hot_Y = np.zeros((Y.size, Y.max() + 1))
-		one_hot_Y[np.arange(Y.size), Y)] = 1
+		one_hot_Y[np.arange(Y.size), Y] = 1
 		one_hot_Y = one_hot_Y.T
 		return one_hot_Y
 		
@@ -65,12 +74,51 @@ class neural_net():
 		self.z1 = self.w1.dot(self.x) + self.b1
 		self.a1 = neural_net.ReLU(self.z1)
 		self.z2 = self.w2.dot(self.a1) + self.b2
-		self.a2 = neural_net.softmax(self.z2)
+		# self.a2 = neural_net.softmax(self.z2)
+		self.a2 = testCode.softmax(self.z2)
+		# print("")
+
+
 
 	def backward_prop(self):
-		# output layer to hidden layer
-		self.dz2 = self.a2 - self.y
-		self.dw2 = self.dz2 
+		m = self.y.size
+		# print(f'm = {m}')
+		self.dz2 = self.a2 - neural_net.one_hot(self.y)
+		self.dw2 = 1 / m * self.dz2.dot(self.a1.T)
+		self.db2 = 1 / m * np.sum(self.dz2)
+
+		self.dz1 = self.w2.T.dot(self.dz2) * neural_net.deriv_ReLU(self.z1)
+		self.dw1 = 1 / m * self.dz1.dot(self.x.T)
+		self.db1 = 1 / m * np.sum(self.dz1)
+
+	def update_params(self, alpha):
+		self.w1 = self.w1 - alpha * self.dw1
+		self.b1 = self.b1 - alpha * self.db1 
+		self.w2 = self.w2 - alpha * self.dw2
+		self.b2 = self.b2 - alpha * self.db2
+	
+	def get_predictions(self):
+		self.predictions = np.argmax(self.a2, 0)
+	
+	def get_accuracy(self):
+		# print(self.predictions, self.y)
+		return np.sum(self.predictions == self.y) / self.y.size
+	
+	def gradient_descent(self, iterations, alpha):
+		for i in range(iterations):
+			self.forward_prop()
+			# self.z1, self.a1, self.z2, self.a2 = testCode.forward_prop(self.w1, self.b1, self.w2, self.b1, self.x)
+			self.backward_prop()
+			# self.dw1, self.db1, self.dw2, self.db2 = testCode.backward_prop(self.z1, self.a1, self.z2, self.a2, self.w1, self.w2, self.x, self.y)
+			self.update_params(alpha)
+			# self.w1, self.b1, self.w2, self.b2 = testCode.update_params(self.w1, self.b1, self.w2, self.b2, self.dw1, self.db1, self.db2, self.db2, alpha=0.1)
+			if i % 10 == 0:
+				self.get_predictions()
+				print(f"iteration {i} accuracy : {self.get_accuracy()}")
+		# return self.w1, self.b1, self.w2, self.b2
+		
+
+				
 
 
 
@@ -80,7 +128,9 @@ train_data = pd.read_csv('../assets/MNIST_CSV/train.csv')
 test_data = pd.read_csv('../assets/MNIST_CSV/test.csv')
 
 nn = neural_net(train_data)
-nn.forward_prop()
+nn.gradient_descent(iterations=5000, alpha=0.1)
+# nn.forward_prop()
+
 
 
 
